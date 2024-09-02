5dcde07ebd34833edc6a0d122aaa217b532066c6
Alex Acciarri <accia.ale@gmail.com>
2024-09-03 00:13:41 +0200

    examples + config

---
+++ config.py
@@ -0,0 +1,17 @@
+# Video settings
+block_size = 8
+width = 1920
+height = 1080
+fps = 24
+
+rgb = 0
+
+# Paths
+import os
+dir = os.getcwd()
+
+src = dir + "/userFiles/source/dataToVid"
+out = dir + "/userFiles/output/video/zipped.zip"
+video_out = dir + "/userFiles/output/video/video.mp4"
+video_in = dir + "/userFiles/source/vidToData/video.mp4"
+zip_out = dir + "/userFiles/output/data/zipped.zip"
